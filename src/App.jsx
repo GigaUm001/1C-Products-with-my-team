a4f88e4828176f5be8a184047f1b895fc80090a3
oktab <aslbekxayrullayev1@gmail.com>
2025-05-31 07:42:41 +0300

    Dlya Snami

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Snami from './Components/S nami/Snami.jsx';
+import Dlya from './Components/Dlya/Dlya.jsx';
+function App() {
+  return (
+    <>
+    <Snami/>
+    <Dlya/>
+    </>
+  )
+}
+
+export default App
